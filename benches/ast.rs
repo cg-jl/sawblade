9f6c74b0b55d7a980df17caf645bf7dd1ff3d491
Cyber Gsus <cyber.gsuscode@gmail.com>
2022-07-01 13:23:30 +0200

    benches/ast: Better name for benching parser

--- benches/ast.rs
+++ benches/ast.rs
@@ -1,6 +1,6 @@
 use criterion::{black_box, Criterion, criterion_main, criterion_group};
 
-pub fn test_sample(c: &mut Criterion) {
+pub fn parse_sample(c: &mut Criterion) {
     let sample = include_str!("../examples/test.abism");
 
     c.bench_function("test sample", |b| {
@@ -8,5 +8,5 @@ pub fn test_sample(c: &mut Criterion) {
     });
 }
 
-criterion_group!(benches, test_sample);
+criterion_group!(benches, parse_sample);
 criterion_main!(benches);
